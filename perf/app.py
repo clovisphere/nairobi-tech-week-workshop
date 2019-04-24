90270e104161b03e15d7b6ce23912d43a771f747
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2019-04-24 09:14:00 +0300

    update

--- perf/app.py
+++ perf/app.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
+from locust import HttpLocust, TaskSet, task
 
+MAX_WAIT_TIME = 5000
+MIN_WAIT_TIME = 5000
 
-class UserAction:
-    pass
+class UserAction(TaskSet):
+    
+    @task(1)
+    def home(self):
+        self.client.get('/index.php')
 
-class User:
-    pass
+    def reserve(self):
+        pass
+
+    def purchase(self):
+        pass
+    
+    def confirmation(self):
+        pass
+
+class User(HttpLocust):
+    task_set = UserAction
+    min_wait = MIN_WAIT_TIME
+    max_wait = MAX_WAIT_TIME
